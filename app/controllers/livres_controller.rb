8dc0da2a41d7cd6ed01f4447daa23bf98e2b21c3
Ava78 <chanie1981@gmail.com>
2015-12-14 23:45:17 +0100

    Ajout formulaire modif du livre

--- app/controllers/livres_controller.rb
+++ app/controllers/livres_controller.rb
@@ -8,13 +8,18 @@ class LivresController < ApplicationController
     @un_livre = Livre.find(params[:id])
   end
   
-  def about
-  end
-
   def create
   	Livre.create titre: params[:titre]
     redirect_to livres_path
   end
+
+  def update
+    @un_livre = Livre.find(params[:id])
+    @un_livre.titre = params[:titre]
+    @un_livre.save
+    redirect_to "/livres/#{params[:id]}"
+  end
+
 
 
 
